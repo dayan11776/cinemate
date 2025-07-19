06e0edff2365abdba7da777735a1a96a172276a0
Bryan <bryantapel619@gmail.com>
2025-07-19 12:30:50 +0800

    “v2”

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,16 +11,10 @@ export const Footer = () => {
                     </Link>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                            <a href="https://www.facebook.com/bryan.tapel" rel="noreferrer" className="hover:underline me-4 md:me-6">Facebook</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <a href="www.linkedin.com/in/bryan-tapel-876271198" rel="noreferrer" className="hover:underline me-4 md:me-6">LinkedIn</a>
                         </li>
                     </ul>
                 </div>
